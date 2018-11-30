153266547c4e91fc7234cc3fca94751b3e65debc
Sameer Ismail <sam@samism.io>
2018-11-30 03:12:46 -0600

    Add routing for protected pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Switch, Route, Redirect } from 'react-router';
+
+import { Auth } from './firebase';
 
 import Weather from './pages/weather';
+import Graph from './pages/historical';
+import SignIn from './pages/auth';
 import Header from './components/Header';
 
 const GlobalStyle = createGlobalStyle`
@@ -25,13 +30,77 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const PageStyle = styled.section`
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
+`;
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return rest.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/signin', state: { from: props.location } }}
+          />
+        );
+      }}
+    />
+  );
+};
+
 class App extends Component {
+  componentWillMount() {
+    Auth.onAuthStateChanged(user => {
+      this.setState({ user: user });
+    });
+  }
+
+  state = {
+    user: undefined,
+    redirectToReferrer: false
+  };
+
   render() {
+    // if (this.state.user === undefined) return <h2>Loading...</h2>;
+
     return (
       <React.Fragment>
         <GlobalStyle />
-        <Header />
-        <Weather />
+        <Header isAuthenticated={!!this.state.user} />
+        <PageStyle>
+          <Switch>
+            <PrivateRoute
+              exact
+              path="/weather"
+              component={Weather}
+              isAuthenticated={!!this.state.user}
+            />
+            <PrivateRoute
+              exact
+              path="/historical"
+              component={Graph}
+              isAuthenticated={!!this.state.user}
+            />
+            <PrivateRoute
+              exact
+              path="/"
+              component={Weather}
+              isAuthenticated={!!this.state.user}
+            />
+            <Route
+              exact
+              path="/signin"
+              render={props => (
+                <SignIn {...props} isAuthenticated={!!this.state.user} />
+              )}
+            />
+          </Switch>
+        </PageStyle>
       </React.Fragment>
     );
   }
