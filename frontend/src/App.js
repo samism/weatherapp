fbe2ff50e044610a2b2ec410abe1e67aa9938b34
Sameer Ismail <sam@samism.io>
2018-11-29 22:55:32 -0600

    Apply global styles to root element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import Weather from './pages/weather';
+import Header from './components/Header';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Arial';
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>test</h1>
-      </div>
+      <React.Fragment>
+        <GlobalStyle />
+        <Header />
+        <Weather />
+      </React.Fragment>
     );
   }
 }
