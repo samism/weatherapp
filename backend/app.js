f72ab0374b08db9e430203f9eb329efdb2d083ce
Sameer Ismail <sam@samism.io>
2018-12-01 05:17:28 -0600

    Enable CORS using npm package

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
 
@@ -9,6 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api', require('./routes/weather'));
 
