218dc5ef6ef9d396f9661b1efdc14cd500414235
Sameer Ismail <sam@samism.io>
2018-12-01 18:26:41 -0600

    Connect to MongoDB with Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +19,20 @@ app.use((err, req, res, next) => {
   console.log(err.message);
   next();
 });
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_CONNECTION_STRING,
+      { useNewUrlParser: true }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connect().catch(error => console.log(`Failed to connect to MongoDB`, error));
+
+console.log('Connected to MongoDB.');
 
 module.exports = app;
